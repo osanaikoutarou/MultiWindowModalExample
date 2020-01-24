a361c896d96f79ea04f66aa2fdb0e88c97a2af02
osanai <osanai.koutarou@mdpr.co.jp>
2020-01-24 10:26:55 +0900

    Initial Commit

---
+++ MultiWindowModalExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiWindowModalExample
+//
+//  Created by 長内幸太郎 on 2020/01/24.
+//  Copyright © 2020 長内幸太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
